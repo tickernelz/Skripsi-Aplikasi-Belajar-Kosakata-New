b8824170e286d397879379075699480c869f7f6a
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-04-29 16:51:14 +0700

    fix sound recreated after going back to Main Menu Scene

--- Assets/backsound.cs
+++ Assets/backsound.cs
@@ -7,6 +7,22 @@ public class backsound : MonoBehaviour
     public AudioSource audioSource;
     public float volume;
     public GameObject soundon, soundoff;
+    private static backsound instance = null;
+    public static backsound Instance
+    {
+        get { return instance; }
+    }
+    
+    void Awake()
+    {
+        if (instance != null && instance != this) {
+            Destroy(this.gameObject);
+            return;
+        } else {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -14,7 +30,6 @@ public class backsound : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = volume;
         audioSource.Play();
-        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
