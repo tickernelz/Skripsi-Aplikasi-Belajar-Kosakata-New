a5a0bdbf116f4638cd18c66a7a01d77c479df539
Zhafron Adani Kautsar <zhafronadani@gmail.com>
2022-05-05 03:44:23 +0700

    add animation in BelajarBab1 Scene and cleanup unused assets
    Signed-off-by: Zhafron Adani Kautsar <zhafronadani@gmail.com>

--- Assets/FirebaseMegaPack-CYKO/FirebaseForUnity - CYKO/Scripts/Constant.cs
+++
@@ -1,6 +0,0 @@
-﻿public class Constant
-{
-
-	public const string FirebaseStorageURL = "gs://belajar-kosakata-arab.appspot.com";
-	public const string FirebaseDatabaseURL = "https://belajar-kosakata-arab-default-rtdb.asia-southeast1.firebasedatabase.app/";
-}
